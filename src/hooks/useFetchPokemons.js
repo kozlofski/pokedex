dedeab02d00459b58340ed3cc1332db070218342
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2025-03-15 17:16:59 +0100

    fetching pokemons' details

--- src/hooks/useFetchPokemons.js
+++ src/hooks/useFetchPokemons.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 const BASE_URL = "https://pokeapi.co/api/v2/";
+const LIMIT = 10;
 
 const useFetchPokemons = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -8,7 +9,7 @@ const useFetchPokemons = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${BASE_URL}pokemon?limit=150`);
+        const response = await fetch(`${BASE_URL}pokemon?limit=${LIMIT}`);
         const jsonResponse = await response.json();
         setPokemons(jsonResponse.results);
         console.log(jsonResponse);
@@ -18,6 +19,36 @@ const useFetchPokemons = () => {
     };
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const pokemonData = [];
+
+    pokemons.forEach(({ url }) => {
+      const fetchPokemon = async () => {
+        // const newPokemon = {};
+        try {
+          const response = await fetch(url);
+          const jsonResponse = await response.json();
+          const {
+            name,
+            base_experience,
+            height,
+            weight,
+            abilities: {
+              0: {
+                ability: { name: abil },
+              },
+            },
+          } = jsonResponse;
+
+          console.log(`pokemon: `, name, base_experience, height, weight, abil);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchPokemon();
+    });
+  }, [pokemons]);
 
   return { pokemons: pokemons };
 };
